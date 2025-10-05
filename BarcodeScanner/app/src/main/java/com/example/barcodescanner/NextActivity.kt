9de1abcc24617e8b44c338bf0b2c3ce841852523
Christopher Ho <cbh0233@mavs.uta.edu>
2025-10-05 15:49:22 -0500

    Working page with buttons

---
+++ BarcodeScanner/app/src/main/java/com/example/barcodescanner/NextActivity.kt
@@ -0,0 +1,13 @@
+package com.example.barcodescanner
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class NextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+    }
+}
